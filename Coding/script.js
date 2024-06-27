48b70d90d1503d74155ca3084afacd5cec95f360
Julien DIAZ <judi8424@gmail.com>
2024-06-27 03:38:21 +0200

    new and changes

---
+++ Coding/script.js
@@ -0,0 +1,8 @@
+let commencer = document.getElementById("commencer");
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    commencer.addEventListener('click', function() {
+        window.location.href = 'connect.html'; // Remplacez par l'URL de la page que vous souhaitez charger
+    });
+});
